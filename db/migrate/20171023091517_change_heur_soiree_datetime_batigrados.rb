c2c5fb3ee449f670df94344df2e51372c96a7284
Fred Reuby <frederic.reubrecht@gmail.com>
2017-12-08 14:01:39 +0100

    ajout confimable à devise

--- db/migrate/20171023091517_change_heur_soiree_datetime_batigrados.rb
+++ db/migrate/20171023091517_change_heur_soiree_datetime_batigrados.rb
@@ -1,5 +1,8 @@
 class ChangeHeurSoireeDatetimeBatigrados < ActiveRecord::Migration[5.0]
-  def change
+  def up
     change_column :batigrados, :heur_soiree, :datetime
+  end
+  def down
+    change_column :batigrados, :heur_soiree, :date
   end
 end
--- db/migrate/20171117144943_change_info_ville_type_in_eleves.rb
+++ db/migrate/20171117144943_change_info_ville_type_in_eleves.rb
@@ -1,5 +1,8 @@
 class ChangeInfoVilleTypeInEleves < ActiveRecord::Migration[5.0]
-  def change
+  def up
     change_column :eleves, :info_ville, :text
+  end
+  def down
+    change_column :eleves, :info_ville, :integer
   end
 end
