235a4558e250a78593be351b06c7c55228c19ae2
Fred Reuby <frederic.reubrecht@gmail.com>
2018-03-24 16:45:49 +0100

    nouvel host dans production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,7 +84,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'test.artandteo.com' }
+  config.action_mailer.default_url_options = { host: 'asso.grupoculturacapoeira.com' }
 
   ActionMailer::Base.delivery_method = :smtp
 
